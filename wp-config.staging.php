c8487c35a1f6d8037953d2c103c5e26bf70d0af6
viktorkattstrom <viktorkattstrom@hotmail.com>
2016-12-01 13:58:20 +0100

    db changes

--- wp-config.staging.php
+++ wp-config.staging.php
@@ -13,16 +13,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', '180958-orgona');
+define('DB_NAME', '180958-orgona2');
 
 /** MySQL database username */
-define('DB_USER', '180958_hb47074');
+define('DB_USER', '180958_ae23604');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'steel1992');
 
 /** MySQL hostname */
-define('DB_HOST', 'orgona-180958.mysql.binero.se');
+define('DB_HOST', 'orgona2-180958.mysql.binero.se');
 
 define('WP_SITEURL', 'http://orgona.mediahelpcrm.se');
 define('WP_HOME', 'http://orgona.mediahelpcrm.se');
